6ac6da7b4a13ba76700790d624397557ada7984c
DC <DC@10.0.0.250>
2017-10-05 19:32:47 -0400

    Signed-off-by:DC <DC@10.0.0.250>

---
+++ CIT260_Game/src/GamePackage/GameMain.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GamePackage;
+
+import byui.cit260.game.model.Crops;
+import byui.cit260.game.model.Game;
+import byui.cit260.game.model.Location;
+import byui.cit260.game.model.Player;
+import byui.cit260.game.model.Map;
+
+/**
+ *
+ * @author DC
+ */
+public class GameMain {
+    
+    public static void main(String[] args){
+        
+        Player playerOne = new Player();
+        playerOne.setName("Fred Flintstone");
+        playerOne.setBestTime(7.00);
+                
+        String playerOneName = playerOne.getName(); 
+        double playerOneTime = playerOne.getBestTime();
+        
+        System.out.println("Name = " + playerOneName + ", time = " + playerOneTime);
+        
+        System.out.println(playerOne.toString());
+    }
+    
+}
