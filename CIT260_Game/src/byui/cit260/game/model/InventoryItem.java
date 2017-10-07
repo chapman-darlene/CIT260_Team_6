3bb7f623f4432534b8bd2b2a1bd41c2224159847
riledesma <30357617+riledesma@users.noreply.github.com>
2017-10-07 15:53:19 -0800

    Add files via upload

---
+++ CIT260_Game/src/byui/cit260/game/model/InventoryItem.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.game.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author rledesma
+ */
+public class InventoryItem implements Serializable {
+    
+     //class instance variables
+     private String description;
+     private int quantity;
+
+    public InventoryItem() {
+    }
+    
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    //set toString, hashCode, equals
+
+    @Override
+    public String toString() {
+        return "InventoryItem{" + "description=" + description + ", quantity=" + quantity + '}';
+    }
+               
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.description);
+        hash = 59 * hash + this.quantity;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InventoryItem other = (InventoryItem) obj;
+        if (this.quantity != other.quantity) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+     
+    
+}
