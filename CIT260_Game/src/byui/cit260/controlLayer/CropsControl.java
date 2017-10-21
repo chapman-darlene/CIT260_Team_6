c7b66619b5dd73e4c2adcfe09610929eb8b74cb9
DC <DC@10.0.0.250>
2017-10-20 22:45:37 -0400

    Signed-off-by:DC <DC@10.0.0.250>

--- CIT260_Game/src/byui/cit260/controlLayer/CropsControl.java
+++ CIT260_Game/src/byui/cit260/controlLayer/CropsControl.java
@@ -20,7 +20,6 @@ public class CropsControl {
     private static Random random = new Random();
     private static final int LAND_BASE = 17;
     private static final int LAND_RANGE = 10;
-    
         
      /*
     calcLandCost() method
@@ -84,7 +83,6 @@ public class CropsControl {
         if(toSell > acres)
         return -1;
     
-    
     //else acresOwned = acresOwned - acresToSell
     acres = acres - toSell;
     theCrops.setAcres(acres);
@@ -98,4 +96,101 @@ public class CropsControl {
         return wheat;     
     }  
     
+    /*Pay Pharaoh Method
+    Purpose: To pay 8% of wheatInStore to Pharaoh
+    Parameters: (wheatInStore, percentageToPharaoh)
+    Returns: wheatInStore
+    Pre-conditions: 
+        Can't pay Pharaoh a negative amount
+        wheatInStore > Pharaoh's amount
+        Have to Force percentage (double) into an int
+    */
+    public int payPharaoh(Crops theCrops, double percentToPharaoh){
+        //define pharaoh's share
+        int paidToPharaoh;
+        int wheat = theCrops.getWheatInStore();
+        //cannot pay pharaoh less than one -- not enough wheat
+        if(wheat <= 9)
+            return -1;
+        //determine amount to pay to Pharaoh
+        //subtract amount paidToPharaoh from wheatInStore
+        paidToPharaoh = (int) (percentToPharaoh * wheat);
+        //System.out.println(paidToPharaoh);
+        wheat = wheat - paidToPharaoh;
+        //System.out.println(wheat);
+        theCrops.setWheatInStore(wheat);
+        return wheat;
+    }
+    
+    //Calculate Random New People Moving In 
+    private static final int NEW_PEOPLE_IN = 5;
+    private static final int NEW_PEOPLE_IN_RANGE = 10;
+ 
+    public int calcNewPeople(Crops theCrops){
+       int people = random.nextInt(NEW_PEOPLE_IN) + NEW_PEOPLE_IN_RANGE; 
+       theCrops.setNewPeople(people);
+       //System.out.println(people);
+       return people;
+    }
+    
+    /*Grow Population Method
+    Purpose: Determine number of new people added to population
+    Parameters: (theCrops)
+    Returns: population
+    Pre-conditions: 
+        if numberWhoDied is greater than 10, return -1
+    */
+    public int growPopulation(Crops theCrops){
+        //Determine number who died
+        int peopleDead = theCrops.getNumberWhoDied();
+        //int newPersons = theCrops.getNewPeople();
+        if(peopleDead >= 10)
+            return -1;
+        //get Random number of new movings
+        int newPersons = theCrops.getNewPeople();
+        //Add number of MoveIns to Population Total
+        int population = theCrops.getPopulation();
+        int newPopulation = (population + newPersons) - peopleDead;
+        theCrops.setPopulation(newPopulation);
+        //return the number of new Population
+        return newPopulation;
+    }
+
+    
+    /*Calc Starved Method
+    Purpose: Determine how many died due to starvation
+    Parameters: (theCrops, wheatPerPerson)
+    Returns: population
+    Pre-conditions: 
+        Can't feed people zero
+        Have to have enough wheatInStore
+    */
+    public int calcStarved(Crops theCrops){
+        int wheatPerPerson = 20;
+        int people = theCrops.getPopulation();
+        int wheat = theCrops.getWheatInStore();
+        int wheatFed = theCrops.getFed();
+        
+        
+        //if (fed< 0) return -1 (Error "Can't feed negative amount of food to people)
+        if(wheatFed <=0)
+            return -1;
+        //if(wheatInStore < fed) return -1 (Error: Not Enough Grain)					
+	if(wheat < wheatFed)
+            return -1;
+        //needFoodAmt = wheatPerPerson * population
+        int neededFood = wheatPerPerson * people;
+        
+        //wheatInStore = wheatInStore - fed	
+        wheat = wheat - wheatFed;
+        theCrops.setWheatInStore(wheat);
+        //numberWhoDied = population - (fed/wheatPerPerson)
+        int peopleDead = people - (wheatFed/wheatPerPerson);
+        theCrops.setNumberWhoDied(peopleDead);
+        //System.out.println(peopleDead);
+        //return = numberWhoDied
+        return peopleDead;
+    }
+    
+   
 }
