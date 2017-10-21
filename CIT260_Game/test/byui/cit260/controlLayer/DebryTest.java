f2e19b078178fc56c114e701ea668f74ef39a2d3
DC <DC@10.0.0.250>
2017-10-20 22:46:13 -0400

    Signed-off-by:DC <DC@10.0.0.250>

--- CIT260_Game/test/byui/cit260/controlLayer/DebryTest.java
+++ CIT260_Game/test/byui/cit260/controlLayer/DebryTest.java
@@ -28,17 +28,18 @@ public class DebryTest {
 
 @Test
     public void testCalcLandCost() {
-        System.out.println("calcLandCost");
+        System.out.println("***calcLandCost Test***");
         int min = 17;
         int max = 26;
         int result = CropsControl.calcLandCost();
-        assertTrue(min <= result && max >= result);    
+        assertTrue(min <= result && max >= result); 
+        //System.out.println(result);
     }
     
 @Test
 public void testBuyLand(){  
     
-        System.out.println("** buyLand test case 1 **");
+        System.out.println("** buyLand Test **");
         
         // we need a crops object with the following values filled in
         Crops theCrops = new Crops();
@@ -52,11 +53,13 @@ public void testBuyLand(){
         int result;
         result = instance.buyLand(theCrops, toBuy, landCost);
         assertEquals(expResult, result);
+        //System.out.println(result);
     }
 
  @Test
     public void testSellLand() {
     // we need a crops object with the following values filled in
+        System.out.println("***sellLand Test***");
         Crops theCrops = new Crops();
         theCrops.setWheatInStore(2000);
         theCrops.setAcres(100);
@@ -66,8 +69,68 @@ public void testBuyLand(){
         CropsControl instance = new CropsControl();
         int expResult = 2050;
         int result = instance.sellLand(theCrops, toSell, landCost);
-        assertEquals(expResult, result);        
+        assertEquals(expResult, result); 
+        //System.out.println(result);
     }
 
+@Test
+    public void testPayPharaoh(){
+       System.out.println("***payPharaoh Test***");
+       Crops theCrops = new Crops();
+       theCrops.setWheatInStore(100);
+       double percentToPharaoh = .10;
+       
+       CropsControl instance = new CropsControl ();
+       int expResult = 90;
+       
+       int result = instance.payPharaoh(theCrops, percentToPharaoh);
+       assertEquals(expResult, result);
+       //System.out.println(result);
+    }
+  
+@Test
+    public void testCalcNewPeople(){
+        System.out.println("***calcNewPeople Test***");
+        Crops theCrops = new Crops();
+        int min = 1;
+        int max = 15;
+        CropsControl instance = new CropsControl();
+        int result = instance.calcNewPeople(theCrops);
+        assertTrue(min <= result && max >= result); 
+        //System.out.println(result);
+    }
+ 
+  
+@Test
+    public void testGrowPopulation(){
+        System.out.println("***GrowPopulation Test***");
+        Crops theCrops = new Crops();
+        theCrops.setPopulation(100);
+        theCrops.setNumberWhoDied(5);
+        theCrops.setNewPeople(10);
+        
+        int expResult = 105;
+        CropsControl instance = new CropsControl();
+        int result = instance.growPopulation(theCrops);
+        assertEquals(expResult, result);
+        
+        //System.out.println(result);
+    }
 
+@Test
+    public void testCalcStarved(){
+        System.out.println("***CalcStarved Test***");
+        Crops theCrops = new Crops();
+        theCrops.setPopulation(100);
+        theCrops.setWheatInStore(2800);
+        theCrops.setFed(2000);
+        
+        CropsControl instance = new CropsControl();
+        int expResult = 0;
+        int result = instance.calcStarved(theCrops);
+        assertEquals(expResult, result);
+        //System.out.println(result);
+        
+        
+    }
 }
