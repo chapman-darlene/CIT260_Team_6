564be926b8de3f7636f1bd01cc263742057a70df
DC <DC@10.0.0.250>
2017-10-24 19:46:54 -0400

    Signed-off-by:DC <DC@10.0.0.250>

---
+++ CIT260_Game/src/byui/cit260/viewLayer/CropsView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.viewLayer;
+
+import byui.cit260.game.model.Crops;
+import byui.cit260.game.model.Game;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DC
+ */
+public class CropsView {
+    //code from DeBry
+    private static Scanner keyboard = new Scanner(System.in);
+    
+    //private static Game theGame = Game.getGame();
+    private static Crops theCrops = Game.getCrop();
+
+}
